b19cc89f60ef80ddb8bb0bd6d77445b36ac4572d
TeLoardBruh <damnloardbruh@gmail.com>
2021-05-28 22:45:40 +0700

    word correction and word segment

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ def read_root():
     return {"Hello": "World"}
 
 
+
+
+
+# word_segmentation 
+
 @app.get("/words/{str}")
 def read_item(str: str, q: Optional[str] = None):
     sym_spell = SymSpell(max_dictionary_edit_distance=0, prefix_length=7)
@@ -39,3 +44,38 @@ def read_item(str: str, q: Optional[str] = None):
     print("{}, {}, {}".format(result.corrected_string, result.distance_sum,
                           result.log_prob_sum))
     return {"str": words,}
+
+
+# word correction
+
+@app.get("/words_correct/{str}")
+def read_item(str: str):
+    sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+    dictionary_path = './files/dict/own_dic.txt'
+    # term_index is the column of the term and count_index is the
+    # column of the term frequency
+    sym_spell.load_dictionary(dictionary_path, 0, 1, encoding="utf8")
+    # print(sym_spell.words.items())
+    # lookup suggestions for single-word input strings
+    input_term = "ខ្ញុំ ចង់"  # misspelling of "members"
+    # max edit distance per lookup
+    # (max_edit_distance_lookup <= max_dictionary_edit_distance)
+    # suggestions = sym_spell.lookup(input_term, Verbosity.CLOSEST,
+    #                            max_edit_distance=2, )
+    # # display suggestion term, term frequency, and edit distance
+    # # display suggestion term, term frequency, and edit distance
+    # print("in here")
+    # for suggestion in suggestions:
+    #     print(suggestion)
+    result = sym_spell.lookup(str, Verbosity.CLOSEST,
+                               max_edit_distance=2)
+    results = []
+    for suggestion in result:
+        results.append(suggestion)
+        # print(suggestion)
+    # words = result.corrected_string
+    # print(result)
+
+    # print("{}, {}, {}".format(result.corrected_string, result.distance_sum,
+    #                       result.log_prob_sum))
+    return {"str": results,'each word': 'word'}
--- romanization.py
+++ romanization.py
@@ -19,26 +19,26 @@ def create_ditc():
 
 
 def check_up():
-    sym_spell = SymSpell(max_dictionary_edit_distance=0, prefix_length=7)
+    sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
     dictionary_path = './files/dict/own_dic.txt'
     # term_index is the column of the term and count_index is the
     # column of the term frequency
     sym_spell.load_dictionary(dictionary_path, 0, 1, encoding="utf8")
     # print(sym_spell.words.items())
     # lookup suggestions for single-word input strings
-    input_term = "ខ្ញុំ ចង់"  # misspelling of "members"
+    input_term = "ចង់"  # misspelling of "members"
     # max edit distance per lookup
     # (max_edit_distance_lookup <= max_dictionary_edit_distance)
-    # suggestions = sym_spell.lookup(input_term, Verbosity.CLOSEST,
-    #                            max_edit_distance=2, )
-    # # display suggestion term, term frequency, and edit distance
-    # # display suggestion term, term frequency, and edit distance
-    # print("in here")
-    # for suggestion in suggestions:
-    #     print(suggestion)
-    result = sym_spell.word_segmentation(input_term)
-    print("{}, {}, {}".format(result.corrected_string, result.distance_sum,
-                          result.log_prob_sum))
+    suggestions = sym_spell.lookup(input_term, Verbosity.CLOSEST,
+                               max_edit_distance=2,include_unknown=True)
+    # display suggestion term, term frequency, and edit distance
+    # display suggestion term, term frequency, and edit distance
+    print("in here")
+    for suggestion in suggestions:
+        print(suggestion)
+    # result = sym_spell.word_segmentation(input_term)
+    # print("{}, {}, {}".format(result.corrected_string, result.distance_sum,
+    #                       result.log_prob_sum))
 
 
 check_up()
