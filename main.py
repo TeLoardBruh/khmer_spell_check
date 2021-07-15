46e0c8efc5ac74af09b911719948ff7144399974
TeLoardBruh <damnloardbruh@gmail.com>
2021-07-16 02:41:54 +0700

    push prep for deploy

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def timeCounter():
 # =========================================
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
